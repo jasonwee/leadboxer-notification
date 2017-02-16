de7010b9059719535edd2bd8fa8a3f275ff8dd36
Jason Wee <peichieh@gmail.com>
2017-02-16 13:07:59 +0800

    added form factory. added how to create the form and saved it.

--- app/controllers/Notification.java
+++ app/controllers/Notification.java
@@ -2,7 +2,12 @@ package controllers;
 
 import java.util.List;
 
+import javax.inject.Inject;
+
+import models.Computer;
 import models.NotificationSpecification;
+import play.data.Form;
+import play.data.FormFactory;
 import play.mvc.Controller;
 import play.mvc.Result;
 
@@ -10,14 +15,46 @@ import static play.libs.Json.toJson;
 
 public class Notification extends Controller {
 	
+	FormFactory formFactory;
+	
+	@Inject
+	public Notification(FormFactory formFactory) {
+		this.formFactory = formFactory;
+	}
+	
+	// c
+	public Result create() {
+		Form<NotificationSpecification> ndForm = formFactory.form(NotificationSpecification.class);
+		return ok(views.html.ndCreateForm.render(ndForm));
+	}
+	
+	// c
+	public Result save() {
+		Form<NotificationSpecification> ndForm = formFactory.form(NotificationSpecification.class).bindFromRequest();
+		if (ndForm.hasErrors()) {
+			return badRequest(views.html.ndCreateForm.render(ndForm));
+		}
+		ndForm.get().save();
+		flash("success", "Notification Specification " + ndForm.get().nKey + " has been created");
+		return ok();
+	}
+	
+	
 	// list all notification specification
+	// r
 	public Result list() {
 		return ok("list content");
 	}
 	
+	// r
 	public Result listAll() {
 		List<NotificationSpecification> notificationSpecifications = NotificationSpecification.findAll();
 		return ok(toJson(notificationSpecifications));
 	}
+	
+	// u
+	
+	
+	// d
 
 }
