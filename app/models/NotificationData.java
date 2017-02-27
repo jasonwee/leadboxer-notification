0a5c4539bfa34078b4edd381d1825717ca5c06df
Jason Wee <peichieh@gmail.com>
2017-02-27 21:38:56 +0800

    initial notification data

---
+++ app/models/NotificationData.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2014 - 2017, LeadBoxer and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  LeadBoxer designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by LeadBoxer in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact LeadBoxer, Herengracht 182 Amsterdam, Noord-Holland 1016 BR
+ * Netherlands or visit www.leadboxer.com if you need additional information or
+ * have any questions.
+ */
+
+package models;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * This is notification data
+ *
+ * @author jason
+ *
+ */
+public class NotificationData {
+
+   // dont bother about encapsulation
+   public String dbTimestamp;
+   public String hitTimestamp;
+   public String datasetId;
+   public String notificationServer;
+   public String UUID;
+   public String logServer;
+   public String value;
+   public String key;
+
+   public String toJsonString() throws JsonProcessingException {
+      ObjectMapper mapper = new ObjectMapper();
+      return mapper.writeValueAsString(this);
+   }
+
+   public static NotificationData toNotificationData(String json) throws JsonParseException, JsonMappingException, IOException {
+      ObjectMapper mapper = new ObjectMapper();
+      return mapper.readValue(json, NotificationData.class);
+   }
+
+   public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
+      String nsString = "{ \"dbTimestamp\":\"\", \"hitTimestamp\":\"2017-02-23 07:02:32\", \"datasetId\":\"e2d1c24722e8a52390a42be6e89f7a65\", \"notificationServer\":null, \"UUID\":\"\", \"logServer\":\"gl04.opentracker.net\", \"value\":\"Bae Systems\", \"key\":\"most_likely_company\" }";
+
+      NotificationData nd = NotificationData.toNotificationData(nsString);
+
+      //System.out.println(nd.hitTimestamp);
+
+      System.out.println(nd.toJsonString());
+   }
+
+
+}
