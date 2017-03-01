43d0e8a32623f296fdadcf1f899d3896b17c52ed
Jason Wee <peichieh@gmail.com>
2017-03-01 18:09:25 +0800

    added unit test docuemntation

---
+++ test/unit/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * Unit testing aims to verify each part of the software by isolating it and then perform tests to demonstrate that each 
+ * individual component is correct in terms of fulfilling requirements and the desired functionality.
+ * 
+ * This type of testing is performed at the earliest stages of the development process, and in many cases it is executed by 
+ * the developers themselves before handing the software over to the testing team.
+ * 
+ * @author jason
+ * 
+ * https://www.tutorialspoint.com/software_testing/software_testing_levels.htm
+ * http://www.softwaretestinghelp.com/types-of-software-testing/
+ * http://reqtest.com/uncategorized/differences-between-different-test-levels/
+ *
+ * in leadboxer context, unit/functional/component , all same , so tests that belong to them are all under unit package
+ */
+package unit;
