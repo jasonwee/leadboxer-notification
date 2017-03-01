fabba2820f92dd7bdb4c88cefbc7755943944351
Jason Wee <peichieh@gmail.com>
2017-03-01 18:07:11 +0800

    added ui test

--- test/integration/IntegrationTest.java
+++ test/integration/IntegrationTest.java
@@ -1,6 +1,6 @@
 package integration;
 
-import org.junit.Test;
+import org.junit.*;
 import play.libs.ws.WS;
 import play.libs.ws.WSClient;
 import play.libs.ws.WSResponse;
@@ -8,11 +8,18 @@ import play.mvc.Http;
 import play.mvc.Result;
 import play.test.WithServer;
 
+import play.test.*;
+
+
 import java.util.concurrent.CompletionStage;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
 import static play.test.Helpers.*;
+
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+
 
 /**
  * Integration testing that involves starting up an application or a server.
@@ -34,6 +41,10 @@ public class IntegrationTest extends WithServer {
         }
     }
 
+    /**
+     * same as testHomePage() but the approach is different.
+     * @throws Exception
+     */
     @Test
     public void testInServerThroughApp() throws Exception {
         // Tests using the internal application available in the server.
@@ -45,5 +56,52 @@ public class IntegrationTest extends WithServer {
         final String body = contentAsString(result);
         assertThat(body, containsString("Notification Specification"));
     }
+    
+    /**
+     * we need to find out how to insert sample data and so we can uncomment the following tests.
+     */
+    @Test
+    public void testUI() {
+        running(testServer(9002, fakeApplication(inMemoryDatabase())), HTMLUNIT, browser -> {
+            browser.goTo("http://localhost:9002");
+
+            assertThat(browser.$("header h1").first().getText(), equalTo("Play application — Notification Specification"));
+            assertThat(browser.$("section h1").first().getText(), equalTo("No notification specifications found"));
+            //assertThat(browser.$("section h1").first().getText(), equalTo("7 notification specifications found"));
+            
+            //assertThat(browser.$("#pagination li.current").first().getText(), equalTo("Displaying 1 to 7 of 7"));
+            //browser.$("#pagination li.next a").click();
+
+            //assertThat(browser.$("#pagination li.current").first().getText(), equalTo("Displaying 11 to 20 of 574"));
+            //browser.$("#searchbox").text("amd");
+            //browser.$("#searchsubmit").click();
+
+            //assertThat(browser.$("section h1").first().getText(), equalTo("One notification specification found"));
+            //browser.$("a", withText("amd")).click();
+
+            //assertThat(browser.$("section h1").first().getText(), equalTo("Edit Notification Specification"));
+
+            //browser.$("#nValue").text("amd123");
+            //browser.$("input.primary").click();
+
+            //assertThat(browser.$("section h1").first().getText(), equalTo("7 notification specifications found"));
+            //assertThat(browser.$(".alert-message").first().getText(), equalTo("Done! Notification Specificationmost_likely_company has been updated"));
+
+            //browser.$("#searchbox").text("amd123");
+            //browser.$("#searchsubmit").click();
+
+            //browser.$("a", withText("Apple II")).click();
+            //browser.$("input.danger").click();
+
+            //assertThat(browser.$("section h1").first().getText(), equalTo("6 notification specifications found"));
+            //assertThat(browser.$(".alert-message").first().getText(), equalTo("Done! Notification Specification has been deleted"));
+
+            //browser.$("#searchbox").text("amd");
+            //browser.$("#searchsubmit").click();
+
+            //assertThat(browser.$("section h1").first().getText(), equalTo("No notification specifications found"));
+        });
+    }
+
 
 }
