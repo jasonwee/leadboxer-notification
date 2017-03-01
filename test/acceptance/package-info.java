ea0a3d98bd538eb3b61806ce470a5fe20fc5707b
Jason Wee <peichieh@gmail.com>
2017-03-01 15:50:29 +0800

    this javadoc served as a guide before dev start write test or where to place the intended test

---
+++ test/acceptance/package-info.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * Acceptance testing is the level in the software testing process where a product is given the green light or not. The aim of 
+ * this type of testing is to evaluate whether the system complies with the end-user requirements and if it is ready for 
+ * deployment.
+ * 
+ * The testing team will utilise a variety of methods, such as pre-written scenarios and test cases to test the software and 
+ * use the results obtained from these tools to find ways in which the system can be improved.
+ * 
+ * The scope of acceptance testing ranges from simply finding spelling mistakes and cosmetic errors, to uncovering bugs 
+ * that could cause a major error in the application.
+ * 
+ * @author jason
+ * 
+ * https://www.tutorialspoint.com/software_testing/software_testing_levels.htm
+ * http://www.softwaretestinghelp.com/types-of-software-testing/
+ * http://reqtest.com/uncategorized/differences-between-different-test-levels/
+ *
+ */
+package acceptance;
---
+++ test/integration/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * Integration testing aims to test different parts of the system in combination in order to assess if they work correctly 
+ * together. By testing the units in groups, any faults in the way they interact together can be identified.
+ * 
+ * There are many ways to test how different components of the system function at their interface; testers can adopt either 
+ * a bottom-up or a top-down integration method.
+ *  
+ * @author jason
+ * 
+ * http://reqtest.com/uncategorized/differences-between-different-test-levels/
+ * http://www.softwaretestinghelp.com/types-of-software-testing/
+ * https://www.tutorialspoint.com/software_testing/software_testing_levels.htm
+ *
+ */
+package integration;
